424d6047a540bbfab5ab18808b8fecccebab0ba0
Dimitrijs Fedotovs <dima@fedoto.ws>
2019-10-17 18:49:14 +0300

    person class

---
+++ src/tsi/notepad/Person.java
@@ -0,0 +1,40 @@
+package tsi.notepad;
+
+public class Person {
+    private int id;
+    private String name;
+    private String surname;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
